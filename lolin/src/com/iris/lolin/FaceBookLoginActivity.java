4c05e0b0feae9f24b49e12e214571e6a58d19df2
park inwoong <dlsdnd345@naver.com>
2014-05-11 03:24:37 +0900

    페이스북 프로필 정보 받기 구현

--- lolin/src/com/iris/lolin/FaceBookLoginActivity.java
+++ lolin/src/com/iris/lolin/FaceBookLoginActivity.java
@@ -3,7 +3,6 @@ package com.iris.lolin;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.DownloadManager.Request;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +10,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import com.facebook.LoggingBehavior;
+import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
@@ -20,18 +20,18 @@ import com.facebook.model.GraphUser;
 
 public class FaceBookLoginActivity extends Activity {
 
-    private Session.StatusCallback statusCallback = new SessionStatusCallback();
-    private Button buttonLoginLogout;
-    
+	private Session.StatusCallback statusCallback = new SessionStatusCallback();
+	private Button buttonLoginLogout;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_facebook_login);
-		
+
 		init();
 		dataInit();
 		facebookInit(savedInstanceState);
-		 
+
 	}
 
 	private void init() {
@@ -44,90 +44,113 @@ public class FaceBookLoginActivity extends Activity {
 		getActionBar().setDisplayShowHomeEnabled(false);
 		getActionBar().setTitle(R.string.board_detail_activity_title);
 	}
-	
+
 	private void facebookInit(Bundle savedInstanceState) {
 		Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
-		
-        Session session = Session.getActiveSession();
-        if (session == null) {
-            if (savedInstanceState != null) {
-                session = Session.restoreSession(this, null, statusCallback, savedInstanceState);
-            }
-            if (session == null) {
-                session = new Session(this);
-            }
-            Session.setActiveSession(session);
-            if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
-                session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
-            }
-        }
-        
-        updateView();
+
+		Session session = Session.getActiveSession();
+		if (session == null) {
+			if (savedInstanceState != null) {
+				session = Session.restoreSession(this, null, statusCallback, savedInstanceState);
+			}
+			if (session == null) {
+				session = new Session(this);
+			}
+			Session.setActiveSession(session);
+			if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
+				session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
+			}
+		}
+
+		updateView();
 	}
 
-	 @Override
-	    public void onStart() {
-	        super.onStart();
-	        Session.getActiveSession().addCallback(statusCallback);
-	    }
-
-	    @Override
-	    public void onStop() {
-	        super.onStop();
-	        Session.getActiveSession().removeCallback(statusCallback);
-	    }
-
-	    @Override
-	    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-	        super.onActivityResult(requestCode, resultCode, data);
-	        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-	    }
-
-	    @Override
-	    protected void onSaveInstanceState(Bundle outState) {
-	        super.onSaveInstanceState(outState);
-	        Session session = Session.getActiveSession();
-	        Session.saveSession(session, outState);
-	    }
-
-	    private void updateView() {
-	        Session session = Session.getActiveSession();
-	        if (session.isOpened()) {
-	            buttonLoginLogout.setText("로그아웃");
-	            buttonLoginLogout.setOnClickListener(new OnClickListener() {
-	                public void onClick(View view) { onClickLogout(); }
-	            });
-	        } else {
-	            buttonLoginLogout.setText("로그인");
-	            buttonLoginLogout.setOnClickListener(new OnClickListener() {
-	                public void onClick(View view) { onClickLogin(); }
-	            });
-	        }
-	    }
-
-	    private void onClickLogin() {
-	        Session session = Session.getActiveSession();
-	        if (!session.isOpened() && !session.isClosed()) {
-	            session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
-	        } else {
-	            Session.openActiveSession(this, true, statusCallback);
-	        }
-	    }
-
-	    private void onClickLogout() {
-	        Session session = Session.getActiveSession();
-	        if (!session.isClosed()) {
-	            session.closeAndClearTokenInformation();
-	        }
-	    }
-
-	    private class SessionStatusCallback implements Session.StatusCallback {
-	        @Override
-	        public void call(Session session, SessionState state, Exception exception) {
-	            updateView();
-	            
-	            
-	        }
-	    }
+	@Override
+	public void onStart() {
+		super.onStart();
+		Session.getActiveSession().addCallback(statusCallback);
 	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		Session.getActiveSession().removeCallback(statusCallback);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		Session session = Session.getActiveSession();
+		Session.saveSession(session, outState);
+	}
+
+	private void updateView() {
+		Session session = Session.getActiveSession();
+		if (session.isOpened()) {
+			buttonLoginLogout.setText("로그아웃");
+			buttonLoginLogout.setOnClickListener(new OnClickListener() {
+				public void onClick(View view) { onClickLogout(); }
+			});
+		} else {
+			buttonLoginLogout.setText("로그인");
+			buttonLoginLogout.setOnClickListener(new OnClickListener() {
+				public void onClick(View view) { onClickLogin(); }
+			});
+		}
+	}
+
+	private void onClickLogin() {
+		Session session = Session.getActiveSession();
+		if (!session.isOpened() && !session.isClosed()) {
+			session.openForRead(new Session.OpenRequest(this).setCallback(statusCallback));
+		} else {
+			Session.openActiveSession(this, true, statusCallback);
+		}
+	}
+
+	private void onClickLogout() {
+		Session session = Session.getActiveSession();
+		if (!session.isClosed()) {
+			session.closeAndClearTokenInformation();
+		}
+	}
+
+	private class SessionStatusCallback implements Session.StatusCallback {
+		@Override
+		public void call(Session session, SessionState state, Exception exception) {
+			updateView();    
+			getFaceBookMe(session);
+		}
+	}
+
+	private void getFaceBookMe(Session session){
+
+		if(session.isOpened()){
+			Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+				@Override
+				public void onCompleted(GraphUser user, Response response) {
+					response.getError();
+					System.err.println(" getId  :  " + user.getId());
+					System.err.println(" getFirstName  :  " + user.getFirstName());
+					System.err.println(" getLastName  :  " + user.getLastName());
+					System.err.println(" getMiddleName  :  " + user.getMiddleName());
+					System.err.println(" getBirthday  :  " + user.getBirthday());
+					System.err.println(" getLink  :  " + user.getLink());
+					System.err.println(" getName  :  " + user.getName());
+					System.err.println(" getUsername :  " + user.getUsername());
+					System.err.println(" getLocation :  " + user.getLocation());
+					System.err.println("getRawResponse  :  " + response.getRawResponse());
+				}
+			}).executeAsync();
+		}
+	}
+
+}
 
